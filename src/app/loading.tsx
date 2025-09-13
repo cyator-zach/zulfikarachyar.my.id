2be8ea95ef3d3106b4feb8562da7485d21e05fa8
Zulfikar Achyar <zulfikaerachyar@gmail.com>
2025-09-13 19:15:17 +0000

    sayaa mau menambahkan animasi loading nih jika sedang ada loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { LoaderCircle } from 'lucide-react';
+
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 z-[100] flex items-center justify-center bg-background/80 backdrop-blur-sm">
+      <LoaderCircle className="h-16 w-16 animate-spin text-primary" />
+    </div>
+  );
+}
